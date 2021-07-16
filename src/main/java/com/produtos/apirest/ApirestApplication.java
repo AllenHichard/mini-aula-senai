5ca129335adcff7d0225d4db162a383c606e4faa
Allen Hichard <allenhichard21@gmail.com>
2021-07-16 17:31:02 -0300

    Inicialização da Aplicação

---
+++ src/main/java/com/produtos/apirest/ApirestApplication.java
@@ -0,0 +1,16 @@
+package com.produtos.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestApplication.class, args);
+	}
+	
+	//http://localhost:8080/swagger-ui.html
+	//http://localhost:8080/h2-console/
+
+}
---
+++ src/test/java/com/produtos/apirest/ApirestApplicationTest.java
@@ -0,0 +1,14 @@
+package com.produtos.apirest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApirestApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
